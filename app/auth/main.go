1db135a28c7b48dfce39e323a549185bb3c50d75
pengjingzhao <1670916827@qq.com>
2025-01-23 23:14:34 +0800

    config: 增加mysql和redis的配置

--- app/auth/main.go
+++ app/auth/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"github.com/PengJingzhao/douyin-commerce/rpc_gen/kitex_gen/auth/authservice"
+	consul "github.com/kitex-contrib/registry-consul"
+	"log"
 	"net"
 	"time"
 
@@ -38,6 +40,13 @@ func kitexInit() (opts []server.Option) {
 	opts = append(opts, server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
 		ServiceName: conf.GetConf().Kitex.Service,
 	}))
+
+	//consul
+	r, err := consul.NewConsulRegister(conf.GetConf().Registry.RegistryAddress[0])
+	if err != nil {
+		log.Fatal(err)
+	}
+	opts = append(opts, server.WithRegistry(r))
 
 	// klog
 	logger := kitexlogrus.NewLogger()
