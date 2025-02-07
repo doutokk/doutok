a91fd3cfc7aae7c4f06fa1d472180e2a84a5208a
suyiiyii <suyiiyii@gmail.com>
2025-02-07 21:44:38 +0800

    feat(order): 实现订单列表查询和标记订单支付功能
    - 在 ListOrderService 中添加订单列表查询逻辑，包括订单项和地址信息
    - 在 MarkOrderPaidService 中实现标记订单支付状态的功能
    - 引入 query 包以使用数据库查询功能

--- app/order/biz/service/list_order.go
+++ app/order/biz/service/list_order.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"github.com/PengJingzhao/douyin-commerce/app/order/biz/dal/query"
+	"github.com/PengJingzhao/douyin-commerce/rpc_gen/kitex_gen/cart"
 
 	order "github.com/PengJingzhao/douyin-commerce/rpc_gen/kitex_gen/order"
 )
@@ -18,6 +20,42 @@ func NewListOrderService(ctx context.Context) *ListOrderService {
 // Run create note info
 func (s *ListOrderService) Run(req *order.ListOrderReq) (resp *order.ListOrderResp, err error) {
 	// Finish your business logic.
-
+	o := query.Q.Order
+	oi := query.Q.OrderItem
+	orders, err := query.Q.Order.Where(o.UserID.Eq(req.UserId)).Find()
+	if err != nil {
+		return nil, err
+	}
+	resp = new(order.ListOrderResp)
+	for _, m := range orders {
+		orderItems, err := query.Q.OrderItem.Where(oi.OrderID.Eq(m.OrderID)).Find()
+		if err != nil {
+			return nil, err
+		}
+		orderItemsResp := make([]*order.OrderItem, 0)
+		for _, orderItem := range orderItems {
+			orderItemsResp = append(orderItemsResp, &order.OrderItem{
+				Item: &cart.CartItem{
+					ProductId: orderItem.ProductID,
+					Quantity:  orderItem.Quantity,
+				},
+				Cost: float32(orderItem.Cost),
+			})
+		}
+		resp.Orders = append(resp.Orders, &order.Order{
+			OrderId:      m.OrderID,
+			UserId:       m.UserID,
+			UserCurrency: m.UserCurrency,
+			Email:        m.Email,
+			Address: &order.Address{
+				StreetAddress: m.StreetAddress,
+				City:          m.City,
+				State:         m.State,
+				Country:       m.Country,
+				ZipCode:       m.ZipCode,
+			},
+			OrderItems: orderItemsResp,
+		})
+	}
 	return
 }
--- app/order/biz/service/mark_order_paid.go
+++ app/order/biz/service/mark_order_paid.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"github.com/PengJingzhao/douyin-commerce/app/order/biz/dal/query"
 	order "github.com/PengJingzhao/douyin-commerce/rpc_gen/kitex_gen/order"
 )
 
@@ -17,6 +18,11 @@ func NewMarkOrderPaidService(ctx context.Context) *MarkOrderPaidService {
 // Run create note info
 func (s *MarkOrderPaidService) Run(req *order.MarkOrderPaidReq) (resp *order.MarkOrderPaidResp, err error) {
 	// Finish your business logic.
+	o := query.Q.Order
+	_, err = query.Q.Order.Where(o.OrderID.Eq(req.OrderId)).Update(o.PaidStatus, true)
+	if err != nil {
+		return nil, err
+	}
 
 	return
 }
