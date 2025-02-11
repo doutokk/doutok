09e6aa974292fbe9250a0fa5d41ce34d46fbf3bc
zbj <3168078770@qq.com>
2025-02-11 17:56:14 +0800

    fix(checkout&payment) : 可观测性，注册中心引入，main.go模板

--- app/checkout/main.go
+++ app/checkout/main.go
@@ -1,25 +1,24 @@
 package main
 
 import (
-	"github.com/PengJingzhao/douyin-commerce/app/checkout/infra/rpc"
-	"github.com/joho/godotenv"
-	"net"
-	"time"
-
 	"github.com/PengJingzhao/douyin-commerce/app/checkout/conf"
+	"github.com/PengJingzhao/douyin-commerce/common/mtl"
+	"github.com/PengJingzhao/douyin-commerce/common/serversuite"
 	"github.com/PengJingzhao/douyin-commerce/rpc_gen/kitex_gen/checkout/checkoutservice"
 	"github.com/cloudwego/kitex/pkg/klog"
-	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	"github.com/joho/godotenv"
 	kitexlogrus "github.com/kitex-contrib/obs-opentelemetry/logging/logrus"
-	"go.uber.org/zap/zapcore"
-	"gopkg.in/natefinch/lumberjack.v2"
+	"net"
+	"os"
 )
+
+var serviceName = conf.GetConf().Kitex.Service
 
 func main() {
 	_ = godotenv.Load()
-	rpc.InitClient()
-
+	mtl.InitTracing(serviceName)
+	mtl.InitMetric(serviceName, "8383", conf.GetConf().Registry.RegistryAddress[0])
 	opts := kitexInit()
 
 	svr := checkoutservice.NewServer(new(CheckoutServiceImpl), opts...)
@@ -31,34 +30,19 @@ func main() {
 }
 
 func kitexInit() (opts []server.Option) {
-	// address
-	addr, err := net.ResolveTCPAddr("tcp", conf.GetConf().Kitex.Address)
-	if err != nil {
-		panic(err)
-	}
-	opts = append(opts, server.WithServiceAddr(addr))
-
-	// service info
-	opts = append(opts, server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{
-		ServiceName: conf.GetConf().Kitex.Service,
-	}))
-
 	// klog
 	logger := kitexlogrus.NewLogger()
 	klog.SetLogger(logger)
 	klog.SetLevel(conf.LogLevel())
-	asyncWriter := &zapcore.BufferedWriteSyncer{
-		WS: zapcore.AddSync(&lumberjack.Logger{
-			Filename:   conf.GetConf().Kitex.LogFileName,
-			MaxSize:    conf.GetConf().Kitex.LogMaxSize,
-			MaxBackups: conf.GetConf().Kitex.LogMaxBackups,
-			MaxAge:     conf.GetConf().Kitex.LogMaxAge,
-		}),
-		FlushInterval: time.Minute,
+	klog.SetOutput(os.Stdout)
+
+	// address
+	address := conf.GetConf().Kitex.Address
+	addr, err := net.ResolveTCPAddr("tcp", address)
+	if err != nil {
+		panic(err)
 	}
-	klog.SetOutput(asyncWriter)
-	server.RegisterShutdownHook(func() {
-		asyncWriter.Sync()
-	})
+	opts = append(opts, server.WithServiceAddr(addr))
+	opts = append(opts, server.WithSuite(serversuite.CommonServerSuite{CurrentServiceName: serviceName, RegistryAddr: conf.GetConf().Registry.RegistryAddress[0]}))
 	return
 }
