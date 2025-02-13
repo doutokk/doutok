fbb10a4f21d03c8d30d145438a84662084c9ec55
zbj <3168078770@qq.com>
2025-02-13 23:41:50 +0800

    feat(user&auth) : 更改配置文件位置

--- app/auth/biz/dal/mysql/init.go
+++ app/auth/biz/dal/mysql/init.go
@@ -3,8 +3,6 @@ package mysql
 import (
 	"fmt"
 	"github.com/doutokk/doutok/app/auth/conf"
-	"os"
-
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
@@ -12,15 +10,13 @@ import (
 var (
 	DB  *gorm.DB
 	err error
+	c   = conf.GetConf()
 )
 
 func Init() {
-	dsn := fmt.Sprintf(conf.GetConf().MySQL.DSN,
-		os.Getenv("MYSQL_USER"),
-		os.Getenv("MYSQL_PASSWORD"),
-		os.Getenv("MYSQL_HOST"),
-		os.Getenv("MYSQL_DATABASE"))
-	DB, err = gorm.Open(mysql.Open(dsn),
+	dsn := "%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+
+	DB, err = gorm.Open(mysql.Open(fmt.Sprintf(dsn, c.MySQL.Username, c.MySQL.Password, c.MySQL.Host, c.MySQL.Port, c.Kitex.Service)),
 		&gorm.Config{
 			PrepareStmt:            true,
 			SkipDefaultTransaction: true,
