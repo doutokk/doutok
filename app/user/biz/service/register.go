8a92dd37c0aff73fd770a90b706c4050dda5273b
suyiiyii <suyiiyii@gmail.com>
2025-02-14 18:09:35 +0800

    feat(user): 实现用户注册功能
    - 添加用户模型和查询接口
    - 实现用户注册的业务逻辑
    - 集成 GORM 和 bcrypt

--- app/user/biz/service/register.go
+++ app/user/biz/service/register.go
@@ -2,7 +2,12 @@ package service
 
 import (
 	"context"
+	"errors"
+	"github.com/doutokk/doutok/app/user/biz/dal/model"
+	"github.com/doutokk/doutok/app/user/biz/dal/query"
 	user "github.com/doutokk/doutok/rpc_gen/kitex_gen/user"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type RegisterService struct {
@@ -17,6 +22,24 @@ func NewRegisterService(ctx context.Context) *RegisterService {
 // Run create note info
 func (s *RegisterService) Run(req *user.RegisterReq) (resp *user.RegisterResp, err error) {
 	// Finish your business logic.
+	if req.Password != req.ConfirmPassword {
+		return nil, errors.New("password and confirm password not match")
+	}
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return
+	}
+	u := &model.User{
+		Email:          req.Email,
+		HashedPassword: string(hashedPassword),
+	}
+	err = query.Q.User.Create(u)
+	if err != nil {
+		return
+	}
+	resp = &user.RegisterResp{
+		UserId: int32(u.ID),
+	}
 
 	return
 }
