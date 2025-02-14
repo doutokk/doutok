c8dbf4bd9b182e2c6f50f4438ef563b09c45fc21
suyiiyii <suyiiyii@gmail.com>
2025-02-14 19:22:22 +0800

    feat(user): 实现用户登录功能

--- app/user/biz/service/login.go
+++ app/user/biz/service/login.go
@@ -2,7 +2,10 @@ package service
 
 import (
 	"context"
+	"errors"
+	"github.com/doutokk/doutok/app/user/biz/dal/query"
 	user "github.com/doutokk/doutok/rpc_gen/kitex_gen/user"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type LoginService struct {
@@ -17,6 +20,20 @@ func NewLoginService(ctx context.Context) *LoginService {
 // Run create note info
 func (s *LoginService) Run(req *user.LoginReq) (resp *user.LoginResp, err error) {
 	// Finish your business logic.
+
+	u := query.User
+	us, err := query.Q.User.Where(u.Email.Eq(req.Email)).First()
+	if err != nil {
+		return nil, errors.New("user not found")
+	}
+
+	if bcrypt.CompareHashAndPassword([]byte(us.HashedPassword), []byte(req.Password)) != nil {
+		return nil, errors.New("password is incorrect")
+	}
+
+	resp = &user.LoginResp{
+		UserId: int32(us.ID),
+	}
 
 	return
 }
