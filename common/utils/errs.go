7fb7c0444b1eed7f344ff0a892653725ca31f75f
Tsukilc <3168078770@qq.com>
2025-02-20 13:14:46 +0000

    feat: 增加异常处理工具

--- common/utils/errs.go
+++ common/utils/errs.go
@@ -1,7 +1,24 @@
 package utils
 
+import (
+	"context"
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/cloudwego/hertz/pkg/protocol/consts"
+)
+
 func PassOrPanic(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func serverError(ctx context.Context, c *app.RequestContext, err error, statusCode ...int) {
+	code := consts.StatusInternalServerError
+	if len(statusCode) > 0 {
+		code = statusCode[0]
+	}
+
+	c.Response.SetStatusCode(code)
+	c.Response.SetBody([]byte(err.Error()))
+	c.Abort()
 }
