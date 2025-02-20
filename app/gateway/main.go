c3471a4cfb0e840c9b0335cd5f9ce706c67e106d
Tsukilc <3168078770@qq.com>
2025-02-20 04:21:53 +0000

    pref(gateway): 静态文件嵌入

--- app/gateway/main.go
+++ app/gateway/main.go
@@ -23,6 +23,7 @@ import (
 	"log"
 	"net"
 	"os"
+	"path/filepath"
 )
 
 var (
@@ -76,6 +77,20 @@ func CasbinMiddleware(e *casbin.Enforcer) app.HandlerFunc {
 }
 
 func writeFile(filePath string, byteFile []byte) {
+
+	// 获取目录路径
+	dirPath := filepath.Dir(filePath)
+
+	// 检查目录是否存在
+	if _, err := os.Stat(dirPath); os.IsNotExist(err) {
+		// 目录不存在，创建目录
+		errr := os.MkdirAll(dirPath, 0755) // 0755 是权限
+		if errr != nil {
+			panic(errr)
+		}
+		fmt.Printf("目录 %s 创建成功\n", dirPath)
+	}
+
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
 		// 如果文件不存在，则创建并写入嵌入的内容
 		errr := ioutil.WriteFile(filePath, byteFile, 0644)
