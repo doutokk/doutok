d82d00891b31c197b5402ba04fae18877b1ced39
suyiiyii <suyiiyii@gmail.com>
2025-02-21 23:05:44 +0800

    make gen-client svc=file

---
+++ rpc_gen/rpc/file/file_client.go
@@ -0,0 +1,46 @@
+package file
+
+import (
+	"context"
+	file "github.com/doutokk/doutok/rpc_gen/kitex_gen/file"
+
+	"github.com/doutokk/doutok/rpc_gen/kitex_gen/file/fileservice"
+	"github.com/cloudwego/kitex/client"
+	"github.com/cloudwego/kitex/client/callopt"
+)
+
+type RPCClient interface {
+	KitexClient() fileservice.Client
+	Service() string
+	UploadFile(ctx context.Context, Req *file.UploadFileReq, callOptions ...callopt.Option) (r *file.UploadFileResp, err error)
+}
+
+func NewRPCClient(dstService string, opts ...client.Option) (RPCClient, error) {
+	kitexClient, err := fileservice.NewClient(dstService, opts...)
+	if err != nil {
+		return nil, err
+	}
+	cli := &clientImpl{
+		service:     dstService,
+		kitexClient: kitexClient,
+	}
+
+	return cli, nil
+}
+
+type clientImpl struct {
+	service     string
+	kitexClient fileservice.Client
+}
+
+func (c *clientImpl) Service() string {
+	return c.service
+}
+
+func (c *clientImpl) KitexClient() fileservice.Client {
+	return c.kitexClient
+}
+
+func (c *clientImpl) UploadFile(ctx context.Context, Req *file.UploadFileReq, callOptions ...callopt.Option) (r *file.UploadFileResp, err error) {
+	return c.kitexClient.UploadFile(ctx, Req, callOptions...)
+}
