41b06cb5797e61b5a69d5f2bfda6b6793d1c77d2
zbj <3168078770@qq.com>
2025-02-24 18:55:58 +0800

    feat(common): 增加通过metadata，从ctx获得userId的函数

--- common/utils/headerHelper.go
+++ common/utils/headerHelper.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"context"
 	"github.com/cloudwego/hertz/pkg/app"
+	"google.golang.org/grpc/metadata"
 	"strconv"
 )
 
@@ -10,6 +11,13 @@ func GetUserIdRequest(c *app.RequestContext) int {
 	strId := c.Request.Header.Get("user_id")
 	userId, _ := strconv.Atoi(string(strId))
 	return userId
+}
+
+func GetUserId(ctx context.Context) int {
+	md, _ := metadata.FromIncomingContext(ctx)
+	userId, _ := md["user_id"]
+	intUserID, _ := strconv.Atoi(userId[0])
+	return intUserID
 }
 
 func GetUserIdFromCtx(ctx context.Context) int {
