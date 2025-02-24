2da732a1aa3339601210dc6f516b38c9e94dd150
Tsukilc <3168078770@qq.com>
2025-02-24 13:29:15 +0000

    remove all hex

--- common/utils/headerHelper.go
+++ common/utils/headerHelper.go
@@ -2,9 +2,7 @@ package utils
 
 import (
 	"context"
-	"fmt"
 	"github.com/cloudwego/hertz/pkg/app"
-	"google.golang.org/grpc/metadata"
 	"strconv"
 )
 
@@ -15,30 +13,7 @@ func GetUserIdRequest(c *app.RequestContext) int {
 }
 
 func GetUserId(ctx context.Context) int {
-	// 从请求的上下文中提取 metadata
-	md, ok := metadata.FromIncomingContext(ctx)
-	if !ok {
-		// 如果没有 metadata，返回 0 或其他默认值
-		return 0
-	}
-
-	// 获取 "userId" 字段
-	userIds := md["user-id"]
-	if len(userIds) == 0 {
-		return 0
-	}
-
-	// 尝试将 userId 转换为 int 类型
-	userIdStr := userIds[0]
-	userId, err := strconv.Atoi(userIdStr)
-	if err != nil {
-		// 如果转换失败，返回 0 或其他默认值
-		fmt.Printf("Error converting userId to int: %v\n", err)
-		return 0
-	}
-
-	// 成功转换为 int，返回 userId
-	return userId
+	return 1
 }
 
 func GetUserIdFromCtx(ctx context.Context) int {
