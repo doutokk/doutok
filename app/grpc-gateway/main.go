a1547228a78c8a8d1eb2fc7ff986e277880a31d1
suyiiyii <suyiiyii@gmail.com>
2025-02-24 16:26:41 +0800

    remove lese hex

--- app/grpc-gateway/main.go
+++ app/grpc-gateway/main.go
@@ -7,16 +7,29 @@ import (
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/metadata"
 
 	cartpb "github.com/doutokk/doutok/app/grpc-gateway/pb/cart"
 )
+
+// 添加认证中间件
+func withAuth() runtime.ServeMuxOption {
+	return runtime.WithMetadata(func(ctx context.Context, req *http.Request) metadata.MD {
+		md := make(map[string]string)
+		if auth := req.Header.Get("Authorization"); auth != "" {
+			md["authorization"] = auth
+		}
+		return metadata.New(md)
+	})
+}
 
 func run() (err error) {
 	ctx := context.Background()
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	mux := runtime.NewServeMux()
+	// 使用自定义选项创建 ServeMux
+	mux := runtime.NewServeMux(withAuth())
 	opts := []grpc.DialOption{grpc.WithTransportCredentials(insecure.NewCredentials())}
 
 	err = cartpb.RegisterCartServiceHandlerFromEndpoint(ctx, mux, "cart-service:8888", opts)
