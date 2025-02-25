bee3584d1b21113a68a297a8ee847908fe17bb55
suyiiyii <suyiiyii@gmail.com>
2025-02-25 21:23:12 +0800

    feat(cart): 修改购物车商品数量逻辑
    - 重构了购物车商品数量更新逻辑
    - 新增删除购物车商品的功能
    -优化了错误处理和异常情况的处理

--- app/cart/biz/service/edit_cart.go
+++ app/cart/biz/service/edit_cart.go
@@ -23,15 +23,16 @@ func (s *EditCartService) Run(req *cart.EditCartReq) (resp *cart.EditCartResp, e
 	ci := query.Q.CartItem
 	userId := uint32(utils.GetUserId(s.ctx))
 	for _, ids := range req.Items {
+
 		item, innerErr := query.Q.CartItem.GetByUserIdAndProductId(userId, ids.ProductId)
 
 		// todo:检查商品是否存在
 
-		nowQuantity := int32(item.Quantity) + ids.Quantity
+		nowQuantity := ids.Quantity
 		if innerErr != nil {
 			innerErr = nil
 
-			// 查数量是否小于0了
+			// 查数量是否小于 0 了
 			if nowQuantity < 0 {
 				return nil, fmt.Errorf("quantity must be greater than 0")
 			}
@@ -41,12 +42,21 @@ func (s *EditCartService) Run(req *cart.EditCartReq) (resp *cart.EditCartResp, e
 				ProductId: ids.ProductId,
 				Quantity:  uint32(nowQuantity),
 			})
+
 		}
 		if innerErr != nil {
 			return nil, innerErr
 		}
+		if ids.Quantity == 0 {
+			// 删除
+			_, innerErr := ci.Where(ci.UserId.Eq(userId), ci.ProductId.Eq(ids.ProductId)).Delete()
+			if innerErr != nil {
+				return nil, innerErr
+			}
+		} else {
 
-		_, innerErr = ci.Where(ci.ID.Eq(item.ID)).Update(ci.Quantity, nowQuantity)
+			_, innerErr = ci.Where(ci.ID.Eq(item.ID)).Update(ci.Quantity, nowQuantity)
+		}
 	}
 
 	return
