237f8e90accfef1f3774db22a082d38d48267255
Tsukilc <3168078770@qq.com>
2025-02-26 15:12:57 +0000

    feat：admin可以编辑商品

--- app/product/biz/service/edit_product.go
+++ app/product/biz/service/edit_product.go
@@ -2,6 +2,8 @@ package service
 
 import (
 	"context"
+	"github.com/doutokk/doutok/app/product/biz/dal/model"
+	"github.com/doutokk/doutok/app/product/biz/dal/query"
 	product "github.com/doutokk/doutok/rpc_gen/kitex_gen/product"
 )
 
@@ -17,6 +19,18 @@ func NewEditProductService(ctx context.Context) *EditProductService {
 // Run create note info
 func (s *EditProductService) Run(req *product.EditProductReq) (resp *product.EditProductResp, err error) {
 	// Finish your business logic.
+
+	product := req.Product
+
+	// 把更改插入数据库
+	_, err = query.Product.WithContext(s.ctx).Where(query.Product.ID.Eq(uint(product.Id))).Updates(model.Product{
+		Name:        product.Name,
+		Price:       product.Price,
+		Description: product.Description,
+		Picture:     product.Picture,
+	})
+
+	// 把整个对象跟数据库替换
 
 	return
 }
