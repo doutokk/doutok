80189b9677155fd5bcf80a7310af5fcfc6002f2c
suyiiyii <suyiiyii@gmail.com>
2025-02-27 20:00:57 +0800

    test(order): 初始化支付模块依赖并优化获取订单测试
    - 初始化 payment 模块的 dal 和 rpc客户端
    - 添加订单请求的测试数据
    - 在上下文中添加用户 ID 元数据

--- app/order/biz/service/get_order_test.go
+++ app/order/biz/service/get_order_test.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"context"
+	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/metadata"
+	"github.com/doutokk/doutok/app/payment/biz/dal"
+	"github.com/doutokk/doutok/app/payment/infra/rpc"
 	order "github.com/doutokk/doutok/rpc_gen/kitex_gen/order"
 	"testing"
 )
@@ -11,7 +14,13 @@ func TestGetOrder_Run(t *testing.T) {
 	s := NewGetOrderService(ctx)
 	// init req and assert value
 
-	req := &order.GetOrderReq{}
+	dal.Init()
+	rpc.InitClient()
+
+	req := &order.GetOrderReq{
+		Id: "fa3e17c8-ff69-c02e-8c0b-855e3ede9255",
+	}
+	metadata.AppendToOutgoingContext(ctx, "user-id", "7")
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
 	t.Logf("resp: %v", resp)
