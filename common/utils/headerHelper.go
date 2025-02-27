78fa8b296b6ed605a05660b6150d1cc6f71171fa
suyiiyii <suyiiyii@gmail.com>
2025-02-27 19:37:58 +0800

    feat(common): 增加用户ID在header中的空值检查 - 在获取用户ID之前，增加了对"User-Id" header的空值检查 - 如果header中不存在用户ID，将抛出panic异常

--- common/utils/headerHelper.go
+++ common/utils/headerHelper.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"context"
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/metadata"
 	"strconv"
 )
@@ -12,6 +13,10 @@ func GetUserId(ctx context.Context) int {
 
 	if !ok {
 		return 0
+	}
+	if len(md.Get("user-id")) == 0 {
+		klog.Errorf("user-id not found in header")
+		panic("user-id not found in header")
 	}
 	userId, _ := strconv.Atoi(md.Get("user-id")[0])
 
