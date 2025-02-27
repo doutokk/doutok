8f89b346de9c7bc3fa52544007f683315fbabff8
suyiiyii <suyiiyii@gmail.com>
2025-02-27 19:49:05 +0800

    user-id 可以通过s.ctx传递吗

--- app/payment/biz/service/start_payment.go
+++ app/payment/biz/service/start_payment.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"context"
+	"errors"
 	"fmt"
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/doutokk/doutok/app/payment/biz/fsm"
 	"github.com/doutokk/doutok/app/payment/infra/rpc"
 	"github.com/doutokk/doutok/common/utils"
@@ -26,13 +28,28 @@ func (s *StartPaymentService) Run(req *payment.StartPaymentReq) (resp *payment.S
 	fmt.Printf("StartPaymentService is called with req: %+v\n", req)
 	userId := utils.GetUserId(s.ctx)
 	oi := req.OrderId
-	amount := 0
-	rpc.OrderClient.GetOrder(s.ctx, &order.GetOrderReq{Id: oi})
-	fsm.NewOrder(fsm.CreatePayOrderReq{
+	r, err := rpc.OrderClient.GetOrder(s.ctx, &order.GetOrderReq{Id: oi})
+	if err != nil {
+		return
+	}
+	if r.Order.UserId != uint32(userId) {
+		klog.Warn("用户名和订单 id 不匹配")
+		return nil, errors.New("")
+	}
+	amount := 0.0
+	for _, item := range r.Order.OrderItems {
+		amount += float64(item.Cost)
+	}
+
+	orderFSM, err := fsm.NewOrder(fsm.CreatePayOrderReq{
 		UserId:  uint32(userId),
 		OrderId: req.OrderId,
 		Amount:  float32(amount),
 	})
+	if err != nil {
+		return nil, err
+	}
+	fmt.Printf("orderFSM: %+v\n", orderFSM)
 
 	return
 }
