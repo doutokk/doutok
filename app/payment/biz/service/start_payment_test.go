9416ccfcc231f91680b5801c94bd08be0f836035
suyiiyii <suyiiyii@gmail.com>
2025-02-28 14:00:18 +0800

    test(payment): 修复测试代码中的 metadata 使用错误
    - 将 "user-id" 的设置移至正确的上下文
    - 使用 kitex 的 metadata 替代 grpc 的 metadata- 调整代码顺序，确保在 Run 方法调用前正确设置上下文

--- app/payment/biz/service/start_payment_test.go
+++ app/payment/biz/service/start_payment_test.go
@@ -2,18 +2,17 @@ package service
 
 import (
 	"context"
+	"github.com/cloudwego/kitex/pkg/remote/trans/nphttp2/metadata"
 	"github.com/doutokk/doutok/app/payment/biz/dal"
 	"github.com/doutokk/doutok/app/payment/biz/dal/mysql"
 	"github.com/doutokk/doutok/app/payment/biz/dal/query"
 	"github.com/doutokk/doutok/app/payment/infra/rpc"
 	payment "github.com/doutokk/doutok/rpc_gen/kitex_gen/payment"
-	"google.golang.org/grpc/metadata"
 	"testing"
 )
 
 func TestStartPayment_Run(t *testing.T) {
 	ctx := context.Background()
-	s := NewStartPaymentService(ctx)
 	// init req and assert value
 
 	dal.Init()
@@ -23,7 +22,8 @@ func TestStartPayment_Run(t *testing.T) {
 	req := &payment.StartPaymentReq{
 		OrderId: "fa3e17c8-ff69-c02e-8c0b-855e3ede9255",
 	}
-	metadata.AppendToOutgoingContext(ctx, "user-id", "7")
+	ctx = metadata.AppendToOutgoingContext(ctx, "user-id", "7")
+	s := NewStartPaymentService(ctx)
 	resp, err := s.Run(req)
 	t.Logf("err: %v", err)
 	t.Logf("resp: %v", resp)
