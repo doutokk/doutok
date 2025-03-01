6a86afdf0651c03ac1f7cc987cfbd710aa56dc84
suyiiyii <suyiiyii@gmail.com>
2025-03-01 22:01:21 +0800

    feat(auth): 添加 token 验证日志
    - 在 token 验证成功时添加日志记录- 在 token 验证失败时添加错误日志记录
    -优化代码注释，提高可读性

--- app/auth/biz/service/verify_token_by_rpc.go
+++ app/auth/biz/service/verify_token_by_rpc.go
@@ -2,6 +2,7 @@ package service
 
 import (
 	"context"
+	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/doutokk/doutok/app/auth/biz/utils"
 	"github.com/doutokk/doutok/app/auth/infra/casbin"
 	"github.com/doutokk/doutok/rpc_gen/kitex_gen/auth"
@@ -24,13 +25,15 @@ func (s *VerifyTokenByRPCService) Run(req *auth.VerifyTokenReq) (resp *auth.Veri
 	var userId int
 
 	if result != nil && err == nil {
+		klog.Warnf("Token validate success: %v %v", userId, token)
 		userId = result.UserID
 	} else {
+		klog.Warnf("Token validate failed: %v %v", err, token)
 		userId = 0
 	}
 
 	var role string
-	// 说明token不合法或者没有token
+	// 说明 token 不合法或者没有 token
 	if userId == 0 {
 		role = "base"
 	} else {
