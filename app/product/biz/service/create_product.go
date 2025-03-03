6275907000df4832ddd6beb4bd5e92aecfc46fff
suyiiyii <suyiiyii@gmail.com>
2025-03-03 12:32:10 +0800

    feat(product): 优化产品创建逻辑，支持分类存在性检查和创建
    - 在创建产品时，先检查分类是否存在
    - 如果分类已存在，则使用现有分类
    - 如果分类不存在，则创建新分类
    - 优化了错误处理和返回值

--- app/product/biz/service/create_product.go
+++ app/product/biz/service/create_product.go
@@ -2,9 +2,12 @@ package service
 
 import (
 	"context"
+	"errors"
+
 	"github.com/doutokk/doutok/app/product/biz/dal/model"
 	"github.com/doutokk/doutok/app/product/biz/dal/query"
 	product "github.com/doutokk/doutok/rpc_gen/kitex_gen/product"
+	"gorm.io/gorm"
 )
 
 type CreateProductService struct {
@@ -21,12 +24,31 @@ func (s *CreateProductService) Run(req *product.CreateProductReq) (resp *product
 	// Finish your business logic.
 
 	p := query.Q.Product
+	pc := query.Q.ProductCategory
 	cats := make([]model.ProductCategory, 0)
-	for _, cat := range req.Categories {
-		cats = append(cats, model.ProductCategory{
-			Name: cat,
-		})
+
+	for _, catName := range req.Categories {
+		// 先查询分类是否存在
+		existingCat, errCat := pc.Where(pc.Name.Eq(catName)).First()
+		if errCat == nil {
+			// 分类已存在，使用现有分类
+			cats = append(cats, *existingCat)
+		} else if errors.Is(errCat, gorm.ErrRecordNotFound) {
+			// 分类不存在，创建新分类
+			newCat := &model.ProductCategory{
+				Name: catName,
+			}
+			errCreate := pc.Create(newCat)
+			if errCreate != nil {
+				return nil, errCreate
+			}
+			cats = append(cats, *newCat)
+		} else {
+			// 其他错误
+			return nil, errCat
+		}
 	}
+
 	m := &model.Product{
 		Name:        req.Name,
 		Description: req.Description,
@@ -36,11 +58,11 @@ func (s *CreateProductService) Run(req *product.CreateProductReq) (resp *product
 	}
 	err = p.Create(m)
 	if err != nil {
-		return
+		return nil, err
 	}
 	resp = &product.CreateProductResp{
 		Id: uint32(m.ID),
 	}
 
-	return
+	return resp, nil
 }
