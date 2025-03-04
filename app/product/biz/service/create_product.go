3b75e0819d8210e50c9fa0c63072b199b9c3def6
Tsukilc <3168078770@qq.com>
2025-03-04 11:59:55 +0000

    feat：创建商品时更新缓存

--- app/product/biz/service/create_product.go
+++ app/product/biz/service/create_product.go
@@ -2,8 +2,11 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/doutokk/doutok/app/product/biz/dal/model"
 	"github.com/doutokk/doutok/app/product/biz/dal/query"
+	"github.com/doutokk/doutok/app/product/biz/dal/redis"
+	"github.com/doutokk/doutok/app/product/constants"
 	product "github.com/doutokk/doutok/rpc_gen/kitex_gen/product"
 )
 
@@ -11,15 +14,11 @@ type CreateProductService struct {
 	ctx context.Context
 }
 
-// NewCreateProductService new CreateProductService
 func NewCreateProductService(ctx context.Context) *CreateProductService {
 	return &CreateProductService{ctx: ctx}
 }
 
-// Run create note info
 func (s *CreateProductService) Run(req *product.CreateProductReq) (resp *product.CreateProductResp, err error) {
-	// Finish your business logic.
-
 	p := query.Q.Product
 	cats := make([]model.ProductCategory, 0)
 	for _, cat := range req.Categories {
@@ -40,6 +39,20 @@ func (s *CreateProductService) Run(req *product.CreateProductReq) (resp *product
 	}
 	resp = &product.CreateProductResp{
 		Id: uint32(m.ID),
+	}
+
+	// 数据库总条数
+	totalCount, err := p.Count()
+	if err != nil {
+		return
+	}
+
+	// 清除最后一页的缓存
+	pageSizes := []int{10, 20, 50, 100}
+	for _, pageSize := range pageSizes {
+		lastPage := (int(totalCount) + pageSize - 1) / pageSize
+		cacheKey := fmt.Sprintf(constants.ProductCategoryKeyPattern, req.Categories[0], lastPage, pageSize)
+		redis.RedisClient.Del(s.ctx, cacheKey)
 	}
 
 	return
