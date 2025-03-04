b6c01846c1d7e2dcd536a140763fbc5a6b1fad6b
Tsukilc <3168078770@qq.com>
2025-03-04 08:58:10 +0000

    feat：生成了一堆东西

--- rpc_gen/rpc/product/product_client.go
+++ rpc_gen/rpc/product/product_client.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/cloudwego/kitex/client"
 	"github.com/cloudwego/kitex/client/callopt"
-	
+	"github.com/doutokk/doutok/rpc_gen/kitex_gen/product/productcatalogservice"
 )
 
 type RPCClient interface {
