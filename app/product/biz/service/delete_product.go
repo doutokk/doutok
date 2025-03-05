a82a977d4d729405d645123f3277f13917d59b74
Tsukilc <3168078770@qq.com>
2025-03-05 08:19:17 +0000

    feat：删除商品时删除es

--- app/product/biz/service/delete_product.go
+++ app/product/biz/service/delete_product.go
@@ -3,6 +3,7 @@ package service
 import (
 	"context"
 	"github.com/doutokk/doutok/app/product/biz/dal/query"
+	"github.com/doutokk/doutok/app/product/infra"
 	product "github.com/doutokk/doutok/rpc_gen/kitex_gen/product"
 )
 
@@ -24,6 +25,12 @@ func (s *DeleteProductService) Run(req *product.DeleteProductReq) (resp *product
 	if err != nil {
 		return &product.DeleteProductResp{Success: false}, err
 	}
+
+	// 删除 Elasticsearch 中的商品
+	if err := infra.DeleteProduct(s.ctx, req.Id); err != nil {
+		return &product.DeleteProductResp{Success: false}, err
+	}
+
 	resp = &product.DeleteProductResp{
 		Success: true,
 	}
