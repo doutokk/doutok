ffd6fcca4ab5f10d1d66b7f1f5a1d7e4b72493eb
Tsukilc <3168078770@qq.com>
2025-03-05 08:38:20 +0000

    feat：添加首页缓存

--- app/product/biz/service/search_products.go
+++ app/product/biz/service/search_products.go
@@ -2,6 +2,9 @@ package service
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/doutokk/doutok/app/product/biz/dal/redis"
 	"github.com/doutokk/doutok/app/product/infra"
 
 	"github.com/doutokk/doutok/rpc_gen/kitex_gen/product"
@@ -22,5 +25,36 @@ func (s *SearchProductsService) Run(req *product.SearchProductsReq) (resp *produ
 		req.Page = 1
 		req.PageSize = 10
 	}
+
+	// 首页是调用的这里的接口，query是null，因此加上缓存
+	if req.Query == "" {
+		// Generate cache key
+		cacheKey := fmt.Sprintf("search_products:page:%d:pageSize:%d", req.Page, req.PageSize)
+
+		// Try to get cached response
+		cachedResp, err := redis.RedisClient.Get(s.ctx, cacheKey).Result()
+		if err == nil {
+			// Unmarshal cached response
+			var cachedResult product.SearchProductsResp
+			if err := json.Unmarshal([]byte(cachedResp), &cachedResult); err == nil {
+				return &cachedResult, nil
+			}
+		}
+
+		// If cache miss or unmarshal error, proceed with search
+		resp, err = infra.SearchProducts(s.ctx, req.Query, "", req.Page, int32(req.PageSize))
+		if err != nil {
+			return nil, err
+		}
+
+		// Cache the response
+		respJSON, err := json.Marshal(resp)
+		if err == nil {
+			redis.RedisClient.Set(s.ctx, cacheKey, respJSON, 0)
+		}
+
+		return resp, nil
+	}
+
 	return infra.SearchProducts(s.ctx, req.Query, "", req.Page, int32(req.PageSize))
 }
